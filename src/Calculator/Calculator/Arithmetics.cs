edd7d7396254b16a6e54ef57114f620abe9cdbe5
Giorgi Kobaidze <george.kobaidze.14@gmail.com>
2023-09-30 16:19:31 +0400

    Add first test for add operation

---
+++ src/Calculator/Calculator/Arithmetics.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator;
+
+public static class Arithmetics
+{
+    public static decimal Add(decimal x, decimal y)
+    {
+        return x + y;
+    }
+
+    public static decimal Subtract(decimal x, decimal y)
+    {
+        return x - y;
+    }
+
+    public static decimal Multiply(decimal x, decimal y)
+    {
+        return x * y;
+    }
+
+    public static decimal Divide(decimal x, decimal y)
+    {
+        return x / y;
+    }
+}
---
+++ tests/Calculator/Calculator.Tests/ArithmeticsTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator.Tests;
+
+public class ArithmeticsTests
+{
+    [Fact]
+    public void Add_SimpleValuesShouldCalculate()
+    {
+        // Arrange
+        var expected = 5m;
+
+        // Act
+        var actual = Arithmetics.Add(3, 2);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
