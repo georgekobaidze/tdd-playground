e4318e3f1d1e2c881814d032d4dfb197726dc64c
Giorgi Kobaidze <george.kobaidze.14@gmail.com>
2023-10-03 19:55:29 +0400

    Add an unit test for adding person to people list

---
+++ tests/Calculator/Samples.Tests/DataAccess/DataAccessTests.cs
@@ -0,0 +1,18 @@
+﻿using Samples.DataAccess;
+
+namespace Samples.Tests.DataAccess;
+
+public class DataAccessTests
+{
+    [Fact]
+    public void AddPersonToPeopleList_ShouldWork()
+    {
+        var newPerson = new Person { FirstName = "Giorgi", LastName = "Kobaidze"};
+        var peopleList = new List<Person>();
+
+        Samples.DataAccess.DataAccess.AddPersonToPeopleList(peopleList, newPerson);
+        
+        Assert.True(peopleList.Count == 1);
+        Assert.Contains(newPerson, peopleList);
+    }
+}
